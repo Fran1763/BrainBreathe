6e326c925ee6b83899f5dd6e00ff82b05014c2dd
Fran1763 <franciscojavier.1763@gmail.com>
2025-06-06 20:12:39 +0200

    jejeje

--- app/src/main/java/com/example/brainbreathe/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/brainbreathe/ui/home/HomeFragment.java
@@ -4,28 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.brainbreathe.databinding.FragmentHomeBinding;
+import com.example.brainbreathe.R; // Asegúrate de importar esto
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.btnStart.setOnClickListener(v -> {
+            NavHostFragment.findNavController(HomeFragment.this)
+                    .navigate(R.id.action_homeFragment_to_dashboardFragment);
+        });
+
         return root;
     }
 
