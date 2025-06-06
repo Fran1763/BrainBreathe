781367983d2df1b60a2cc0252a11bf7393fe1f51
alvaroramosjmz <alvramjim@gmail.com>
2025-06-06 13:49:06 +0200

    jojojo commit

--- app/src/main/java/com/example/brainbreathe/MainActivity.java
+++ app/src/main/java/com/example/brainbreathe/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private int pimpinela;
+    private int jojojo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
