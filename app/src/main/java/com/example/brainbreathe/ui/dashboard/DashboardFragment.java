01b4f058fa572c4dd15337475bc092ee93e5d317
alvaroramosjmz <alvramjim@gmail.com>
2025-06-06 19:03:32 +0200

    antes de las fotos

--- app/src/main/java/com/example/brainbreathe/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/brainbreathe/ui/dashboard/DashboardFragment.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.ImageButton;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,7 +21,7 @@ public class DashboardFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
+        /*DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
@@ -27,6 +30,23 @@ public class DashboardFragment extends Fragment {
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
+        */
+
+        // Inflar el diseño para este fragmento
+        View view = inflater.inflate(R.layout.fragment_moodflash, container, false);
+
+        // Configurar listeners para los emojis
+        ImageButton emojiHappy = view.findViewById(R.id.emojiHappy);
+        emojiHappy.setOnClickListener(v -> {
+            // Lógica para el estado de ánimo "Feliz"
+        });
+
+        // Repetir para los demás emojis...
+
+        return view;
+
+
     }
 
     @Override
